e026bac0c94134a9e7e0438b5f67d02d3d98190e
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2025-10-08 11:53:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,26 @@
-import re, os, time
-from os import environ, getenv
-id_pattern = re.compile(r'^.\d+$') 
+import os
+import time
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config(object):
-    # pyro client config
-    API_ID    = os.environ.get("API_ID", "238902")
-    API_HASH  = os.environ.get("API_HASH", "da7e86c7b0e6220b8a9e0aed228a68")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7365322253:AAHboQtxtJ2Ov5E8xqBvbmz2fG82Atrw") 
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    # database config
-    DB_NAME = os.environ.get("DB_NAME","codeflix")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://public:public@cluster0.aisg0rh.mongodb.net/?retryWrites=true&w=majority")
-    PORT = os.environ.get("PORT", "25")
- 
-    # other configs
-    BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
-    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots,codeflixsupport,anime_cruise_netflix').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
-    
-    # wes response configuration     
-    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+    DB_NAME = os.environ.get("DB_NAME", "codeflix")
+    DB_URL = os.environ.get("DB_URL")
+
+    ADMIN = [int(admin) for admin in os.environ.get('ADMIN', '').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+
+    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', '').split(',')
+    START_PIC = os.environ.get("START_PIC")
+
+    BOT_UPTIME = time.time()
+    WEBHOOK = os.environ.get("WEBHOOK", "False").lower() in ("true", "1", "t")
+    PORT = int(os.environ.get("PORT", 8080))
 
 
 class Txt(object):
